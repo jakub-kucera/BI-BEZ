469cc18387fd695d75bb5a8bf6256b463d4d0742
Jakub Kučera <kucerj56@fit.cvut.cz>
2021-03-17 21:46:14 +0100

    Changed code to pure C. Fixed minor errors.

---
+++ task2_hash/project/main.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <string.h>
+#include <openssl/evp.h>
+
+static const int INPUT_ARRAY_SIZE = 48;
+
+int main(int argc, char *argv[]) {
+
+    int numOfZeros;
+    /* check input argument */
+    if (argc != 2
+        || sscanf(argv[1], " %d ", &numOfZeros) != 1
+        || numOfZeros < 0
+        || numOfZeros > 384) {
+        return 6;
+    }
+    time_t t;
+    /* initializes rand function */
+    srand((unsigned) time(&t));
+
+    /* 48*8=384; input size == output size; using smaller input size might cause inability to generate wanted output values */
+    unsigned char inputToHash[INPUT_ARRAY_SIZE];
+    char hashFunction[] = "sha384";  /* zvolena hashovaci funkce ("sha1", "md5", ...) */
+
+    EVP_MD_CTX *ctx;  /* struktura kontextu */
+    const EVP_MD *type; /* typ pouzite hashovaci funkce  */
+    unsigned char hash[EVP_MAX_MD_SIZE]; /* char pole pro hash - 64 bytu (max pro sha 512)  */
+    unsigned int length;  /* vysledna delka hashe  */
+
+    /* Inicializace OpenSSL hash funkci */
+    OpenSSL_add_all_digests();
+    /* Zjisteni, jaka hashovaci funkce ma byt pouzita */
+    type = EVP_get_digestbyname(hashFunction);
+
+    /* Pokud predchozi prirazeni vratilo -1, tak nebyla zadana spravne hashovaci funkce */
+    if (!type) {
+        printf("Hash %s neexistuje.\n", hashFunction);
+        return 1;
+    }
+
+    /* create context for hashing  */
+    ctx = EVP_MD_CTX_new();
+    if (ctx == NULL)
+        return 2;
+
+
+    /* init input array */
+    for (int i = 0; i < INPUT_ARRAY_SIZE; i++) {
+        inputToHash[i] = 0;
+    }
+
+    while (1) {
+
+        inputToHash[rand() % INPUT_ARRAY_SIZE]++; /* increments random element in array  */
+
+        /* Hash the text */
+        if (!EVP_DigestInit_ex(ctx, type, NULL)) /* context setup for our hash type */
+            return 3;
+
+        if (!EVP_DigestUpdate(ctx, inputToHash, INPUT_ARRAY_SIZE)) /* feed the message in */
+            return 4;
+
+        if (!EVP_DigestFinal_ex(ctx, hash, &length)) /* get the hash */
+            return 5;
+
+
+        /* bool nextIteration = false; */
+        uint8_t nextIteration = 0;
+        /* goes by entire bytes */
+        for (int i = 0; i < numOfZeros / 8; i++) {
+            if (hash[i] != 0x00) {
+                nextIteration++;
+                break;
+            }
+        }
+        /* goes through the rest */
+        if (!nextIteration && hash[numOfZeros / 8] >> (8 - (numOfZeros % 8)) == 0x00) {
+            break;
+        }
+    }
+
+
+    EVP_MD_CTX_free(ctx); /* destroy the context */
+
+    /* Write hash function input data*/
+    for (int i = 0; i < INPUT_ARRAY_SIZE; i++) {
+        printf("%02x", inputToHash[i]);
+    }
+    printf("\n");
+
+    /* Write output hash */
+    for (unsigned int i = 0; i < length; i++)
+        printf("%02x", hash[i]);
+    printf("\n");
+    return 0;
+}
